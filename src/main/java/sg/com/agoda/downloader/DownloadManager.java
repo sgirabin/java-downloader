c145e4f53ef15f37df367ab750257c4eba98aade
Isak Rabin <isak_r@yahoo.com>
2016-06-19 22:34:05 +0800

    Project Setup

--- src/main/java/sg/com/agoda/downloader/DownloadManager.java
+++ src/main/java/sg/com/agoda/downloader/DownloadManager.java
@@ -3,14 +3,20 @@
  */
 package sg.com.agoda.downloader;
 
+import java.io.IOException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  *
  * @author isakrabin
  */
+
 public class DownloadManager {
-    
-    public static void main(String[] args) {
- 
+
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        
     }
-    
+
 }
