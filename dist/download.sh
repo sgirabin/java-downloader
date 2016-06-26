7799dd23a86fc984e9ede590ebdd088a45c71831
Isak Rabin <isak_r@yahoo.com>
2016-06-26 23:43:46 +0800

    add binary version

--- dist/download.sh
+++ dist/download.sh
@@ -3,6 +3,5 @@ export JAVA_HOME=/usr/
 export PATH=$PATH:$JAVA_HOME/bin
 export CLASSPATH=$JAVA_HOME/jre/lib/ext:$JAVA_HOME/lib/tools.jar
 
-
 BASEDIR=$(dirname "$0")
 $JAVA_HOME/bin/java -server -Xms2048m -Xmx2048m -XX:MaxMetaspaceSize=2048m -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -cp "$BASEDIR/classes:$BASEDIR/lib/*:." sg.com.agoda.downloader.DownloadApp $* 
